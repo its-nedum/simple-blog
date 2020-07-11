dd094903254cffc907d727a3950c92f34be9aa4c
its-nedum <de.magnificent1@gmail.com>
2020-07-11 22:22:57 +0100

    chores(successfully changed the home route)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+    def index
+    end
+
+    def new
+        # This loads the form to create a new post
+    end
+
+    def create
+        # This method create the new post
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
