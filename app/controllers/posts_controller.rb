b790224596c8a4c26956d1f3bcfaa6df27595aff
its-nedum <de.magnificent1@gmail.com>
2020-07-11 22:36:55 +0100

    chores(added more posts view)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,26 @@ class PostsController < ApplicationController
     end
 
     def new
-        # This loads the form to create a new post
+        # This loads the page to create a new post
     end
 
     def create
         # This method create the new post
     end
 
+    def show
+        # This loads a single post
+    end
+
+    def edit
+        # This loads the edit page
+    end
+
     def update
+        # This method update the
     end
 
     def destroy
+        # This delete a post
     end
 end
