6d00b8ebc3424c6ccdc74e7ddbff6f0831f2da18
its-nedum <de.magnificent1@gmail.com>
2020-07-13 12:49:42 +0100

    features(implemented comments on post)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    # This method create a comment
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to post_path(@post)
+    end
+
+    private def comment_params
+        params.require(:comment).permit(:username, :body)
+    end
 end
